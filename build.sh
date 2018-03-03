99f60c19834c46f9f81118e0a445f4060d95f3dc
chirimenmonster <chirimenmonster@users.noreply.github.com>
2018-03-03 19:16:54 +0900

    change build script

--- build.sh
+++ build.sh
@@ -1,18 +1,17 @@
 #! /bin/bash
 
-MODBASE=scripts/client/gui/mods
 MODNAME=chirimen.replserver_0.1.1
-PKGVERSION=0.1.1
+PKGVER=0.1.1
 
 rm -r tmp
 mkdir tmp
 
 python2 package.py
-cp build/$MODNAME.wotmod tmp
+cp build/${MODNAME}.wotmod tmp
 
 python2 -m compileall client/client.py
 mv client/client.pyc tmp
 
 cp LICENSE.txt README.md tmp
 
-( cd tmp; zip -r replserver.zip . )
+( cd tmp; zip -r replserver-${PKGVER}.zip . )
